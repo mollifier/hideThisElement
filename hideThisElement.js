6915eb2bf08c712187b6b702ad941f04ab9a55a7
mollifier <mollifier@gmail.com>
2010-02-09 00:58:19 +0900

    メタ情報を追加

--- hideThisElement.js
+++ hideThisElement.js
@@ -1,5 +1,10 @@
-// Jetpack feature
-// hideThisElement
+// @name: Hide This Element
+// @description: Add context menu to hide target element.
+// @auther : mollifier http://d.hatena.ne.jp/mollifier/
+// @version : 0.0.1
+//
+// Hide This Element is released under the MIT License.
+// http://www.opensource.org/licenses/mit-license.php
 
 jetpack.future.import("menu");
 
